2a6d74fffff96c5a769a937b3ab12b2356be9190
yukiyalien <mmtk@yukiya.me>
2020-06-05 23:13:32 +0900

    つくった

---
+++ README.md
@@ -0,0 +1,18 @@
+# kyunkyun-generator
+
+![Netlify](https://img.shields.io/netlify/aaaaaaaaaaa) ![GitHub release (latest by date)](https://img.shields.io/github/v/release/yukiyalien/gero-generator)
+
+インターネットに吐瀉物を放流するためのサイトです
+
+以下をコピーしたりトゥートしたりツイートしたりできます
+
+
+```
+エ🤮ン🤮ト🤮マ🤮おぇ〜っおぇっおぇオェオェオェオェオェOeOeOeOeおぇっエントマッオェュオェオェﾝ🤮オェッ🤮オェッオェッオェ🤮オェオェﾝ🤮
+```
+
+Let's 嘔吐！ → https://gero.yukiya.me/
+
+# See also
+
+- https://github.com/yukiyalien/kyunkyun-generator
---
+++ script.js
@@ -0,0 +1,20 @@
+const gero = `エ🤮ン🤮ト🤮マ🤮おぇ〜っおぇっおぇオェオェオェオェオェOeOeOeOeおぇっエントマッオェュオェオェﾝ🤮オェッ🤮オェッオェッオェ🤮オェオェﾝ🤮
+#嘔吐マ
+https://gero.yukiya.me/`;
+const encodedGero = encodeURIComponent(gero);
+
+const openShare = () => {
+  const domain = document.getElementById('server-domain').value;
+  const link = `https://${domain}/share?text=${encodedGero}`;
+
+  window.open(link, '', 'width=500,height=400');
+};
+
+// ページ読み込み完了時にロード
+window.onload = () => {
+  // kyun-link classのついたリンクに対して文字列を挿入
+  Array.from(document.getElementsByClassName('gero-link')).forEach(element => {
+    element.href = element.href.replace('{gero}', encodedGero);
+  });
+};
+window.openShare = openShare;
